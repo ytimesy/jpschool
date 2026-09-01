376c56d573d27177fef3fc8e493a58bf93751719
ytimesy <yukio.juna1224@gmail.com>
2026-09-01 15:51:42 +0900

    Fix production boot configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
   config.consider_all_requests_local = false
 
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.active_storage.service = :local
   config.active_support.report_deprecations = false
 
   config.force_ssl = true
